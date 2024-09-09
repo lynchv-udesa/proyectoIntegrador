dca91ae8e74bb4ecd0deebbf355fcdc8ac9e7c4a
lynchv-udesa <lynchv@udesa.edu.ar>
2024-09-09 12:29:15 -0300

    inicio

---
+++ proyecto-integrador/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function Footer () {
+    return (
+        <footer>
+        <p>Giuliana Gargiulo - Matias Abalo Pico - Victoria Lynch</p>
+    </footer>
+    )
+}
+
+export default Footer
---
+++ proyecto-integrador/src/screens/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Home(){
+    return (
+        <>
+            <React.Fragment>
+            
+            <h1>Peliculas</h1>
+            <main>
+                <h2>Mas Populares</h2>
+                <h2>Estrenos</h2>
+            </main>
+            <Footer/>
+            </React.Fragment>
+        </>
+    )
+}
+export default Home
